50454c10bdceff24e627aab74bd3bf46c29549be
Naveen Reddy <naveen.reddy@hsmumsz.local>
2018-01-03 10:09:57 +0530

    data added

---
+++ src/redux/reducer/init_state.js
@@ -0,0 +1,10 @@
+export default {
+	login:{
+		username: ['naveenkumar@gmail.com','abc@gmail.com'],
+		password: ['naveen', 'abcd'],
+		username1: '',
+		password1: '',
+		dataValidation: false,
+		errorPopup: false
+	}
+}
